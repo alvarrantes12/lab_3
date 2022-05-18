02c5d4f95c7cac3c62f1f501b201733365c67b3e
alearal12 <alejandro.araya@ucrso.info>
2022-05-17 20:14:27 -0600

    Base terminada, faltan traducciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_010323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "e_mail"
+    t.integer "personal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.boolean "pedigree"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "owner_id", null: false
+    t.index ["owner_id"], name: "index_pets_on_owner_id"
+  end
+
+  add_foreign_key "pets", "owners"
 end
